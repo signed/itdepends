f545d5bdd3002240bcd9253252b30e39f338e158
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-01-27 21:33:41 +0100

    initial project setup

---
+++ src/main/kotlin/com/github/signed/itdepends/itdepends/ItdependsApplication.kt
@@ -0,0 +1,11 @@
+package com.github.signed.itdepends.itdepends
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ItdependsApplication
+
+fun main(args: Array<String>) {
+    runApplication<ItdependsApplication>(*args)
+}
